50f6756235183126f0a838b1d82e20472a2cd3d1
Daniel Taylor <daktaylor98@gmail.com>
2024-04-29 18:47:09 +0000

    added comment posting system

--- smartcare_django/smartcare_appointments/views/appointment_views.py
+++ smartcare_django/smartcare_appointments/views/appointment_views.py
@@ -1,5 +1,5 @@
-import datetime
-
+import json
+from datetime import datetime, date
 from django.db import models
 from smartcare_auth.models import User
 from rest_framework import viewsets, mixins
@@ -40,12 +40,34 @@ class AppointmentView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Ret
     @action(detail=True, methods=['post'])
     def approve(self, request, pk=None):
         appointment = self.get_object()
+
+        # update appointment stage
+        appointment.stage = AppointmentStage.APPROVED
+        appointment.save()
+
+        comment = AppointmentComment.objects.create(
+            created_by=request.user,
+            appointment=appointment,
+            text="Appointment Approved"
+        )
+        comment.save()
+
+        # try to schedule
+
         schedule_result = scheduler(appointment)
 
         if schedule_result:
+
+            comment = AppointmentComment.objects.create(
+                created_by=request.user,
+                appointment=appointment,
+                text=f"Appointment automatically scheduled at {datetime.strftime(appointment.assigned_start_time, '%d/%m/%y %H:%M')} with {appointment.staff.first_name} {appointment.staff.last_name}"
+            )
+            comment.save()
+
             return Response({"result" : True, "message": "appointment scheduled"})
 
-        return Response({"result" : False, "message": "failed to schedule appointment"})
+        return Response({"result" : False, "message": "failed to automatically schedule appointment"})
 
     @action(detail=True, methods=['post'])
     def reject(self, request, pk=None):
@@ -61,7 +83,7 @@ class AppointmentView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Ret
         # if appointment.staff != requested_user:
         #    return Response({"result" : "error", "message" : "the logged in user does not own this appointment"})
 
-        appointment.actual_start_time = datetime.datetime.now()
+        appointment.actual_start_time = datetime.now()
         appointment.save()
 
         comment = AppointmentComment.objects.create(
@@ -83,7 +105,7 @@ class AppointmentView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Ret
         # if appointment.staff != requested_user:
         #    return Response({"result" : "error", "message" : "the logged in user does not own this appointment"})
 
-        appointment.actual_end_time = datetime.datetime.now()
+        appointment.actual_end_time = datetime.now()
         appointment.stage = AppointmentStage.COMPLETED
         appointment.save()
 
@@ -107,6 +129,25 @@ class AppointmentView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Ret
 
         return Response({"result" : "success"})
 
+    @action(detail=True, methods=['post'])
+    def add_comment(self, request, pk=None):
+        appointment = self.get_object()
+        comment = json.loads(request.body)['comment']
+        
+        if not comment:
+            return Response({"result" : "error", "message" : "cannot add empty comment"})
+
+        if self.request.user != appointment.patient and not self.request.user.is_clinic_staff():
+            return Response({"result" : "error", "message" : "cannot add comment to another appointment"})
+
+        comment = AppointmentComment.objects.create(
+            created_by=request.user,
+            appointment=appointment,
+            text=comment
+        )
+        comment.save()
+
+        return Response({"result" : "success"})
 
 class AppointmentCommentView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = AppointmentComment.objects.all()
