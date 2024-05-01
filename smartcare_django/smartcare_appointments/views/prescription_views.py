62a7f2c439443fbfcb1b3a3cadf2796346ae22db
Alasdair <alasdair2.freeman@live.uwe.ac.uk>
2024-05-01 16:58:19 +0100

    Users bring able to request new repeat perscriptions, progress to doctors being able to approve prescriptions

--- smartcare_django/smartcare_appointments/views/prescription_views.py
+++ smartcare_django/smartcare_appointments/views/prescription_views.py
@@ -47,6 +47,9 @@ class PrescriptionRequestView(mixins.ListModelMixin, mixins.CreateModelMixin, mi
         else:
             return [IsStaffOrExternal()]
     '''
+    @action(detail=False)
+    def prescription_requests(self, request):
+        pass
     
     #To do: @action(detail=False, methods=['post']) Make another action with repeat perscription serializer fo patients to create a new repeat prescription request
     @action(detail=False, methods=['post'])
@@ -57,19 +60,49 @@ class PrescriptionRequestView(mixins.ListModelMixin, mixins.CreateModelMixin, mi
         except KeyError:
             return Response({"result" : False, "message": "could not locate prescription"}, status=status.HTTP_400_BAD_REQUEST)
         
-        prescription = Prescription.objects.get(pk=prescription_id)
+        for i in prescription_id:
+            prescription = Prescription.objects.get(pk=i)
 
-        if user.is_clinic_staff():
-            return Response({"result" : False, "message": "cannot create request for non patient user"})
-        
-        if not prescription:
-            return Response({"result" : False, "message": "failed to get prescription"})
-        
-        prescription_request = PrescriptionRequest()
-        prescription_request.prescription = prescription
-        prescription_request.requested_time = datetime.now(timezone.utc)
-        prescription_request.save()
+            if user.is_clinic_staff():
+                return Response({"result" : False, "message": "cannot create request for non patient user"})
+            
+            if not prescription:
+                return Response({"result" : False, "message": "failed to get prescription"})
+
+            print(prescription_id)
+            
+            prescription_request = PrescriptionRequest()
+            prescription_request.prescription = prescription
+            prescription_request.requested_time = datetime.now(timezone.utc)
+            prescription_request.save()
         
         return Response({"result" : True, "message": "success"})
 
     #To do: @action(detail=False, methods=['post']) Make another action for doctors to be able to accept or reject prescription requests
+    @action(detail=False, methods=['post'])
+    def respond_request(self, request):
+        user = request.user
+        try:
+            prescription_id = request.data.pop("prescription_id")
+        except KeyError:
+            return Response({"result" : False, "message": "could not locate prescription"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        for i in prescription_id:
+            prescription = Prescription.objects.get(pk=i)
+
+            if not user.is_clinic_staff():
+                return Response({"result" : False, "message": "only clinic staff can respond to prescription requests"})
+            
+            if not prescription:
+                return Response({"result" : False, "message": "failed to get prescription"})
+            
+            print(prescription_id)
+            
+            prescription_request = PrescriptionRequest()
+            prescription_request.approved_by = user
+            prescription_request.approved_time = datetime.now(timezone.utc)
+            
+            prescription_request.save()
+            
+        
+
