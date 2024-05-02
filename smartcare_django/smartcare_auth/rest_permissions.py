1b72d701b230687687f54dbe91b7785185c4cb4c
Alasdair <alasdair2.freeman@live.uwe.ac.uk>
2024-05-02 08:36:18 +0100

    Giving doctors the ability to create wholly new prescriptions

--- smartcare_django/smartcare_auth/rest_permissions.py
+++ smartcare_django/smartcare_auth/rest_permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from smartcare_auth.models import UserType
+
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -22,3 +24,16 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         
         return obj == request.user
+    
+
+class PrescriptionRequestIsOwnerOrStaffOrExternal(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user and request.user.is_authenticated and request.user.is_clinic_or_external_staff():
+            return True
+        
+        return obj.prescription.patient == request.user
+    
+
+class IsAuthenticatedAndNotExternal(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.user_type != UserType.EXTERNAL
